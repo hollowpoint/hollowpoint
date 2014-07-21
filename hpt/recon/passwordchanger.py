caeba0bcdb2c5da358835e3684fa40ab7f6ce217
jathanism <jathan@gmail.com>
2014-07-21 01:40:33 -0700

    Got passwordchanger working for Aruba, NetScreen, and PAN

--- hpt/recon/passwordchanger.py
+++ hpt/recon/passwordchanger.py
@@ -5,13 +5,14 @@
 
 import collections
 from functools import wraps
+from twisted.python import log
 from trigger.cmds import Commando
 from trigger.netdevices import NetDevices
 from trigger.utils import crypt_md5
 import xml.etree.cElementTree as ET
 
-import logging
-log = logging.getLogger(__name__)
+#import logging
+#log = logging.getLogger(__name__)
 
 # Namedtuple for use by command-emitting functions that return a 2-tuple of
 # (cmds, status)
@@ -23,19 +24,20 @@ class PasswordChanger(Commando):
 
     ``work`` should be a dictionary of {'device_name': 'password', ...}
     """
-    def __init__(self, work, *args, **kwargs):
-        self.work = work
+    def __init__(self, devices, new_password, *args, **kwargs):
+        #self.work = work
+        self.new_password = new_password
         self.failures = {}
         self.production_only = kwargs.get('production_only', True)
         self.nd = NetDevices(with_acls=False, production_only=self.production_only)
         self._super = super(PasswordChanger, self)
-        devices = self.work.keys()
+        #devices = self.work.keys()
         self._super.__init__(devices=devices, *args, **kwargs)
 
     def errback(self, reason, dev):
         """Catch the error and store the result."""
         self.failures[dev] = reason
-        log.info('FAILURE: %s' % dev)
+        log.msg('FAILURE: %s' % dev)
 
     def ioslike(self, generate):
         """
@@ -76,6 +78,11 @@ class PasswordChanger(Commando):
     def to_arista(self, device, commands=None, extra=None):
         cmd = 'aaa root secret %s'
         return [cmd]
+    
+    def to_aruba(self, device, commands=None, extra=None):
+        cmd = 'mgmt-user admin %s' % self.new_password
+        cmds ['configure terminal', cmd, 'exit', 'commit apply']
+        return cmds
 
     def to_brocade(self, device, commands=None, extra=None):
         # Brute force that this is a VDX switch
@@ -119,7 +126,7 @@ class PasswordChanger(Commando):
         </rpc>
         <rpc><commit-configuration/></rpc>
         """
-        newpass = self.work[device]
+        #newpass = self.work[device]
 
         # Create lock-configuration element
         xml = [ET.Element('lock-configuration')]
@@ -127,12 +134,12 @@ class PasswordChanger(Commando):
 
         # Recursively add subelements to 'load-configuration' element. When
         # finished, set text to inner-most element ('encrypted-password') to the
-        # value of newpass.
+        # value of self.new_password.
         load = ET.Element('load-configuration', action='replace')
         body = ET.SubElement(load, 'configuration')
         syst = ET.SubElement(body, 'system')
         root = ET.SubElement(syst, 'root-authentication')
-        ET.SubElement(root, 'encrypted-password').text = crypt_md5(newpass)
+        ET.SubElement(root, 'encrypted-password').text = crypt_md5(self.new_password)
         status.extend(['changing password', 'done'])
 
         # Tack on config/commit to output
@@ -142,6 +149,21 @@ class PasswordChanger(Commando):
 
         #print 'Sending %r to %s' % (xml, device)
         return xml
+
+    def to_netscreen(self,device, commands=None, extra=None):
+        cmd = 'set admin password %s' % self.new_password
+        return [cmd, 'save config']
+
+    def to_paloalto(self, device, commands=None, extra=None):
+        """
+        On PAN devices the password change takes effect immediately and does not
+        require a commit.
+        """
+        crypt_pw = crypt_md5(self.new_password)
+        cmd = 'set mgt-config users admin phash %s' % crypt_pw
+
+        cmds = ['configure', cmd]
+        return cmds
 
     def generate(self, device, commands=None, extra=None):
         """
@@ -164,7 +186,7 @@ class PasswordChanger(Commando):
         Use string replacement to insert the new password into the command
         string for each device.
         """
-        newpass = self.work[device]
+        #newpass = self.work[device]
         #print 'Setting password to %r on %s' % (newpass, device)
         cmds = gen(device, commands, extra)
 
@@ -172,102 +194,19 @@ class PasswordChanger(Commando):
         # found in it. Brute force? You betcha!
         for idx, cmd in enumerate(cmds):
             if '%s' in cmd:
-                cmds[idx] = cmd % newpass
+                cmds[idx] = cmd % self.new_password
                 break
         return cmds
 
     def parse(self, results, device):
         """Overload what to do when things complete"""
-        log.info('SUCCESS: %s' % device)
+        log.msg('SUCCESS: %s' % device)
 
-    # NYI because we haven't figured out how to bake in incrementals into the
-    # Commando subclasses just yet. Not that it's impossible, just time
-    # consuiming.
-    """
-    def update_board(results, dev=None):
-        try:
-            self.active[dev] = status[len(results)]
-        except IndexError:
-            pass
-
-    def move_on(res, dev=None):
-        if not dev:
-            return None
-        del self.active[dev]
-
-    def complete(results, dev=None, extra=None):
-        log.msg('SUCCESS: %s' % dev)
-
-    def run_and_draw(self, stdscr, work, jobs, failures):
-        active = {}
-        start_qlen = len(work)
-        start_time = time.time()
-
-        if not stdscr:
-            def redraw():
-                pass
-
-        else:
-            def redraw():
-                draw_screen(stdscr, work, active, failures, start_qlen,
-                            start_time)
-
-    def run(self):
-        self.run_and_draw()
-        self._super.run()
-    """
-
-
-def min_sec(secs):
-    secs = int(secs)
-    return '%d:%02d' % (secs / 60, secs % 60)
-
-# NYI
-"""
-import curses
-def draw_screen(s, work, active, failures, start_qlen, start_time):
-    '''
-    Curses-based status board.
-    '''
-    s.erase()
-
-    # DO NOT cache the result of s.getmaxyx(), or you cause race conditions
-    # which can create exceptions when the window is resized.
-    def maxx():
-        y, x = s.getmaxyx()
-        return x
-    def maxy():
-        y, x = s.getmaxyx()
-        return y
-
-    s.addstr(0, 0, 'camaro_pc'[:maxx()], curses.A_BOLD)
-    progress = '  %d/%d devices' % (start_qlen - len(work) - len(active),
-                                    start_qlen)
-    s.addstr(0, maxx() - len(progress), progress)
-
-    doneness = 1 - float(len(work) + len(active)) / start_qlen
-    elapsed = raw_time() - start_time
-    elapsed_str = min_sec(elapsed)
-    if doneness == 0:
-        remaining_str = ' '
-    elif doneness == 1:
-        remaining_str = 'done'
-    else:
-        remaining_str = min_sec(elapsed / doneness - elapsed)
-    max_line = int(maxx() - len(remaining_str) - len(elapsed_str) - 2)
-
-    s.addstr(1, 0, elapsed_str)
-    s.addstr(1, maxx() - len(remaining_str), remaining_str)
-    s.hline(1, len(elapsed_str) + 1, curses.ACS_HLINE, int(max_line * doneness))
-
-    if failures:
-        count, plural = len(failures), (len(failures) > 1 and 's' or '')
-        s.addstr(2, 0, ' %d failure%s, will report at end ' % (count, plural),
-                 curses.A_STANDOUT)
-
-    for y, (dev, status) in zip(range(3, maxy()), active.items()):
-        s.addstr(y, 0, ('%s: %s' % (dev, status))[:maxx()], curses.A_BOLD)
-
-    s.move(maxy() - 1, maxx() - 1)
-    s.refresh()
-"""
+if __name__ == '__main__':
+    log.startLogging(open('/tmp/pw.log', 'w'), setStdout=False)
+    #devices = ['firewall']
+    devices = ['netscreen']
+    new_password = 'abc123'
+    pw = PasswordChanger(devices=devices, new_password=new_password,
+            verbose=True)
+    pw.run()
