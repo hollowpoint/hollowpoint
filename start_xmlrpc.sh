bc8cf07298120c82594476c02b44944659cf0af4
jathanism <jathan@gmail.com>
2014-08-19 22:38:27 -0700

    Update start_xmlrpc.sh to work w/ trigger_loader in its current form

--- start_xmlrpc.sh
+++ start_xmlrpc.sh
@@ -6,7 +6,7 @@ killall twistd
 # Start the mofo
 TWISTD=`which twistd`
 #$TWISTD -l trigger-xmlrpc.log --pidfile /var/run/trigger-xmlrpc.pid trigger-xmlrpc
-$TWISTD -l trigger-xmlrpc.log trigger-xmlrpc -p 9000 -s 9001
+PYTHONPATH=hpt $TWISTD -l trigger-xmlrpc.log trigger-xmlrpc -p 9000 -s 9001
 
 # Watch the log
 tail -F trigger-xmlrpc.log
